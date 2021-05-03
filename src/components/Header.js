c998467e2e68ecca05a5d3f5af844aec236a45cb
Joseph Soria <josephsoria6@gmail.com>
2021-05-02 19:01:26 -0500

    removed React import, changed classnames

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,9 @@
-import React from 'react';
-
 function Header({ heading }) {
   return (
-    <div className="row d-flex justify-content-center my-3">
-      <div className="col-11">
+    <div className="row d-flex justify-content-center">
+      <header className="mb-2 mx-2">
         <h1 className="text-center">{heading}</h1>
-      </div>
+      </header>
     </div>
   )
 }
