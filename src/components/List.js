cb2a0a884c70c5f35d9173386efc329f0e18ee2e
Joseph Soria <josephsoria6@gmail.com>
2021-06-27 22:15:35 -0500

    changed buttons to anchor tags, added ids to lists

--- src/components/List.js
+++ src/components/List.js
@@ -1,37 +1,30 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function List({ items, name }) {
-  const [classes,setClasses] = useState("list-group d-none")
+  const [classes, setClasses] = useState("list-group d-none");
 
   const handleToggle = () => {
     if (classes === "list-group d-none") {
-      setClasses("list-group")
+      setClasses("list-group");
     } else {
-      setClasses("list-group d-none")
+      setClasses("list-group d-none");
     }
-  }
+  };
 
   return (
     <div className="row">
-      <button
-        className="btn"
-        type="button"
-        onClick={handleToggle}
-      >
-      {name}
-      </button>
-      <ul className={classes}>
-        {items?.map(item => (
-          <li
-            key={item}
-            className="list-group-item text-center"
-          >
+      <a className="btn mb-2" href={"#" + name.split(" ")[0]} onClick={handleToggle}>
+        {name}
+      </a>
+      <ul className={classes} id={name.split(" ")[0]}>
+        {items?.map((item) => (
+          <li key={item} className="list-group-item text-center">
             {item}
           </li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
 export default List;
