871171996a3ecfb966f0a4efc33bca0a99ec70b7
Joseph Soria <josephsoria6@gmail.com>
2021-09-05 14:44:40 -0500

    Made Section component

---
+++ src/components/Section.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Segment } from "semantic-ui-react";
+import "./Section.css";
+
+const Section = () => {
+  return (
+    <Segment raised >
+      <h1>This is a section</h1>
+    </Segment>
+  );
+};
+
+export default Section;
