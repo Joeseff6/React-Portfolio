e6071cabcaf1f04053783f1cc09c032b211a4229
Joseph Soria <josephsoria6@gmail.com>
2021-09-05 12:27:47 -0500

    Made AboutMeSection component

---
+++ src/components/AboutMeSection.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Section = () => {
+  return (
+    <div className="w-full h-1/3 mb-4" style={{ border: "black 2px solid" }}>
+      <h1 className="text-center">About me section</h1>
+    </div>
+  );
+};
+
+export default Section;
