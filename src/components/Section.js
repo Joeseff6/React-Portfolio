4e6684d3f1391917d466b66b8dd6643a807aa0b2
Joseph Soria <josephsoria6@gmail.com>
2021-09-06 14:07:01 -0500

    Wrote fade in functionality using React Bootstrap

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
 import Row from "react-bootstrap/Row";
+import Fade from "react-bootstrap/Fade";
 import "./Section.css";
 
 const Section = ({ children }) => {
+  const [ fadeIn, setFadeIn ] = useState(false);
+
+  const delayFadeIn = () => {
+    setTimeout(() => {
+      setFadeIn(true);
+    }, 500)
+  }
+
   return (
-    <Row className="section mb-5">
-      <h1>This component is used to wrap the sections</h1>
-      {children}
-    </Row>
-  )
-}
+    <Fade appear={true} in={fadeIn} onEnter={delayFadeIn()}>
+      <Row className="section mb-5">
+        <h1>This component is used to wrap the sections</h1>
+        {children}
+      </Row>
+    </Fade>
+  );
+};
 
 export default Section;
