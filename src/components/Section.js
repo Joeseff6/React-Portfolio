d86425b474e44bbd8dee83ea3fde066fb3221e7d
Joseph Soria <josephsoria6@gmail.com>
2021-09-06 13:48:44 -0500

    Imported Row and Section.css, returned Row component and children prop

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Row from "react-bootstrap/Row";
+import "./Section.css";
 
-const Section = () => {
+const Section = ({ children }) => {
   return (
-    <div>
+    <Row className="section mb-5">
       <h1>This component is used to wrap the sections</h1>
-    </div>
+      {children}
+    </Row>
   )
 }
 
