982309dbae8286596ada67746959784812b3e542
Joseph Soria <josephsoria6@gmail.com>
2021-09-06 11:54:02 -0500

    Divided sections into columns with Grid

--- src/components/AboutMeSection.js
+++ src/components/AboutMeSection.js
@@ -1,10 +1,25 @@
 import React from "react";
-import Image from"./Image";
+import Image from "./Image";
+import { Segment, Grid } from "semantic-ui-react";
 
-const Section = () => {
+const AboutMeSection = () => {
   return (
-    <h1>This is a section</h1>
+    <Segment raised>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={6} style={{border:"black 2px solid"}}>
+            <h1>This is the image</h1>
+          </Grid.Column>
+          <Grid.Column width={5} style={{border:"black 2px solid"}}>
+            <h1>This is some text</h1>
+          </Grid.Column>
+          <Grid.Column width={5} style={{border:"black 2px solid"}}>
+            <h1>This is even more text</h1>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
   );
 };
 
-export default Section;
+export default AboutMeSection;
