4f1b60b3f727e3974c6773946e0eedc93df98c26
Joseph Soria <josephsoria6@gmail.com>
2021-09-06 15:53:48 -0500

    Created Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+
+class Nav extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <Navbar bg="light" expand="md" fixed="top" >
+      </Navbar>
+    )
+  }
+}
+
+export default Nav;
