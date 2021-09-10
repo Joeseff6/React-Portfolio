2bae45b29749cf4523b0e51941ad8c87b7f9ac1f
Joseph Soria <josephsoria6@gmail.com>
2021-09-09 23:32:14 -0500

    Added showOverflow state to change bio text overflow when hovering over text

--- src/components/AboutMeSection.js
+++ src/components/AboutMeSection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "react-bootstrap/Image";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -8,6 +8,16 @@ import Header from "./Header";
 import List from "./List";
 
 const AboutMeSection = () => {
+  const [ showOverflow, setShowOverflow ] = useState("overflow-hidden");
+
+  const onHover = () => {
+    setShowOverflow("overflow-auto");
+  }
+
+  const onLeaveHover = () => {
+    setShowOverflow("overflow-hidden");
+  }
+
   return (
     <>
       <Col className="p-0 h-100 d-flex" md={3}>
@@ -20,7 +30,7 @@ const AboutMeSection = () => {
         <Row>
           <Col>
             <Header title="My Bio" fontSize={2} verticalMargin={3}/>
-            <p className="fs-5 px-5 lh-base overflow-auto bio">
+            <p className={`fs-5 px-5 lh-base ${showOverflow} bio`} onMouseEnter={onHover} onMouseLeave={onLeaveHover}>
               <span className="indent">Hi!</span> My name is Joseph Soria, and I am a graduate from the
               University of Houston. I majored in Mechanical Engineering, and have worked in the Oil and Gas industry
               since 2011. I am currently developing my Full Stack Engineering skills, with my main focus in 
