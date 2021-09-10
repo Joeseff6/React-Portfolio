7e94913e669d0786d96c0998ab01762d8c73fb42
Joseph Soria <josephsoria6@gmail.com>
2021-09-09 22:27:56 -0500

    Imported Header component

--- src/components/AboutMeSection.js
+++ src/components/AboutMeSection.js
@@ -4,24 +4,25 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ProfilePic from "../assets/images/Profilepic.jpg";
 import Container from "react-bootstrap/Container";
+import Header from "./Header";
 
 const AboutMeSection = () => {
   return (
     <>
-      <Col className="p-0 h-100 d-flex" md={3} >
+      <Col className="p-0 h-100 d-flex" md={3}>
         <Image src={ProfilePic} fluid rounded className="h-100 flex-grow-1" />
       </Col>
       <Col>
         <Row>
-          <h1 className="my-2 fs-1 text-center" style={{border: "black 2px solid"}}>Who Am I?</h1>
+          <Header title="Who Am I?" fontSize={1} />
         </Row>
-        <Row style={{border: "black 2px solid"}}>
+        <Row style={{ border: "black 2px solid" }}>
           <Col>
-            <h1 className="mb-2 fs-2 text-center">My Bio</h1>
+            <Header title="My Bio" fontSize={2} />
             <p className="fs-5 px-5">This is some text </p>
           </Col>
           <Col>
-            <h1 className="mb-2 fs-2 text-center">Contact Me</h1>
+            <Header title="Contact Me" fontSize={2} />
           </Col>
         </Row>
       </Col>
