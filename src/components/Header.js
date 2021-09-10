531bf2e3bccdb242bd8f79ef0ac38469987f33d6
Joseph Soria <josephsoria6@gmail.com>
2021-09-09 23:20:31 -0500

    Added verticalMargin prop, specified default props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
-const Header = ({ title, fontSize }) => {
+const Header = ({ title, fontSize, verticalMargin }) => {
   return (
-    <h1 className={`my-2 fs-${fontSize} text-center`}>{title}</h1>
-  )
-}
+    <h1 className={`my-${verticalMargin} fs-${fontSize} text-center`}>
+      {title}
+    </h1>
+  );
+};
+
+Header.defaultProps = {
+  title: "This is a header",
+  fontSize: 1,
+  verticalMargin: 2,
+};
 
 export default Header;
