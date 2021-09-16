0c687a3b2bb37097febc39998ff5163c21517d59
Joseph Soria <josephsoria6@gmail.com>
2021-09-15 21:17:16 -0500

    Made FadeComponent

---
+++ src/components/FadeComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class FadeComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { fadeIn: false };
+  }
+}
+
+export default FadeComponent;
