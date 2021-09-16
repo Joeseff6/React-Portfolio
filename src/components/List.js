5cfdbad7b11eaadc8ec0f81b09dd4ff3b8f986ee
Joseph Soria <josephsoria6@gmail.com>
2021-09-15 19:10:41 -0500

    Created List component

---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+import ListGroup from "react-bootstrap/ListGroup";
+
+const List = () => {
+  return (
+    <ListGroup>
+      <ListGroup.Item>One Item</ListGroup.Item>
+      <ListGroup.Item>Two Item</ListGroup.Item>
+      <ListGroup.Item>Three Item</ListGroup.Item>
+      <ListGroup.Item>Four Item</ListGroup.Item>
+    </ListGroup>
+  )
+}
+
+export default List;
