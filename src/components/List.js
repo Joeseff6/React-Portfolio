e8478a17851618db291af235115b8a4aacb17d88
Joseph Soria <josephsoria6@gmail.com>
2021-09-15 19:29:58 -0500

    Added state to transform text, changed list items

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,46 @@
 import ListGroup from "react-bootstrap/ListGroup";
+import { useState } from "react";
 
 const List = () => {
+  const [transformText, setTransformText] = useState(false);
+
+  const handleHover = () => {
+    setTransformText(!transformText);
+  };
+
+  const handleHLeaveHover = () => {
+    setTransformText(!transformText);
+  };
+
   return (
-    <ListGroup>
-      <ListGroup.Item>One Item</ListGroup.Item>
-      <ListGroup.Item>Two Item</ListGroup.Item>
-      <ListGroup.Item>Three Item</ListGroup.Item>
-      <ListGroup.Item>Four Item</ListGroup.Item>
+    <ListGroup className="text-center">
+      <ListGroup.Item>
+        Email:{" "}
+        <a href="mailto: josephsoria6@gmail.com">josephsoria6@gmail.com</a>
+      </ListGroup.Item>
+      <ListGroup.Item
+        onMouseEnter={handleHover}
+        onMouseLeave={handleHLeaveHover}
+      >
+        Phone: {!transformText ? "Hover to display!" : "832-657-9060"}
+      </ListGroup.Item>
+      <ListGroup.Item>
+        Github:{" "}
+        <a href="https://github.com/Joeseff6" target="_blank">
+          www.github.com/joeseff6
+        </a>
+      </ListGroup.Item>
+      <ListGroup.Item>
+        LinkedIn:{" "}
+        <a
+          href="https://linkedin.com/in/joseph-soria-5a335845/"
+          target="_blank"
+        >
+          https://www.linkedin.com/in/joseph-soria-5a335845/
+        </a>
+      </ListGroup.Item>
     </ListGroup>
-  )
-}
+  );
+};
 
 export default List;
