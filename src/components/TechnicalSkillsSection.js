6f5478bf467444fa5cd130b50e27ea4a121d1e94
Joseph Soria <josephsoria6@gmail.com>
2021-09-17 15:43:53 -0500

    Wrote layout for component

--- src/components/TechnicalSkillsSection.js
+++ src/components/TechnicalSkillsSection.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Header from "./Header";
+import FadeComponent from "./FadeComponent";
+import CollapseComponent from "./CollapseComponent";
 
 const TechnicalSkillsSection = () => {
   return (
-    <div>
-      <h1>This is the technical skills section</h1>
-    </div>
+    <>
+      <Col>
+        <Row>
+          <h1>This is the heading</h1>
+        </Row>
+        <Row>
+          <Col>
+            <h1>These are my Technical Skills</h1>
+          </Col>
+          <Col>
+            <h1>These are my preferred positions</h1>
+          </Col>
+        </Row>
+      </Col>
+      <Col>
+        <h1>This is an image</h1>
+      </Col>
+    </>
   );
 };
 
