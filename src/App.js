d301136c5848fafd2dc1ea26f834cd723d517649
Joseph Soria <josephsoria6@gmail.com>
2021-09-16 20:40:46 -0500

    imported FadeComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./App.css";
+import Container from "react-bootstrap/Container";
+import Section from "./components/Section";
+import Nav from "./components/Nav";
+import AboutMeSection from "./components/AboutMeSection";
+import FadeComponent from "./components/FadeComponent";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>This is a React component</h1>
-      </div>
-    )
+      <Container fluid>
+        {/* <Nav /> */}
+        <FadeComponent fadeTimer={500}>
+          <Section fadeDirection="right">
+            <AboutMeSection />
+          </Section>
+        </FadeComponent>
+        <FadeComponent fadeTimer={1000}>
+          <Section fadeDirection="left"></Section>
+        </FadeComponent>
+        <FadeComponent fadeTimer={1500}>
+          <Section fadeDirection="right"></Section>
+        </FadeComponent>
+      </Container>
+    );
   }
 }
