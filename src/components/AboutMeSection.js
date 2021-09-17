02e4089be9cdcfbe0afc3fca72a8c195be2da008
Joseph Soria <josephsoria6@gmail.com>
2021-09-16 21:27:35 -0500

    Added icons for list items

--- src/components/AboutMeSection.js
+++ src/components/AboutMeSection.js
@@ -3,9 +3,10 @@ import Image from "react-bootstrap/Image";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ProfilePic from "../assets/images/Profilepic.jpg";
-import Container from "react-bootstrap/Container";
 import Header from "./Header";
 import List from "./List";
+import FadeComponent from "./FadeComponent";
+import "./AboutMeSection.css";
 
 const AboutMeSection = () => {
   const [showOverflow, setShowOverflow] = useState("overflow-hidden");
@@ -27,28 +28,30 @@ const AboutMeSection = () => {
         <Row>
           <Header title="Who Am I?" fontSize={1} />
         </Row>
-        <Row>
-          <Col>
-            <Header title="My Bio" fontSize={2} verticalMargin={3} />
-            <p
-              className={`fs-5 px-4 lh-base ${showOverflow} bio`}
-              onMouseEnter={onHover}
-              onMouseLeave={onLeaveHover}
-            >
-              <span className="indent">Hi!</span> My name is Joseph Soria, and I
-              am a graduate from the University of Houston. I majored in
-              Mechanical Engineering, and have worked in the Oil and Gas
-              industry since 2011. I am currently developing my Full Stack
-              Engineering skills, with my main focus in Web Development using
-              React. My Hobbies include shooting pool, trading in the stock
-              market, and coding of course!
-            </p>
-          </Col>
-          <Col>
-            <Header title="Contact Me" fontSize={2} verticalMargin={3} />
-            <List />
-          </Col>
-        </Row>
+        <FadeComponent fadeTimer={750}>
+          <Row>
+            <Col>
+              <Header title="My Bio" fontSize={2} verticalMargin={3} />
+              <p
+                className={`fs-5 px-4 lh-base ${showOverflow} bio`}
+                onMouseEnter={onHover}
+                onMouseLeave={onLeaveHover}
+              >
+                <span className="indent">Hi!</span> My name is Joseph Soria, and
+                I am a graduate from the University of Houston. I majored in
+                Mechanical Engineering, and have worked in the Oil and Gas
+                industry since 2011. I am currently developing my Full Stack
+                Engineering skills, with my main focus in Web Development using
+                React. My Hobbies include shooting pool, trading in the stock
+                market, and coding of course!
+              </p>
+            </Col>
+            <Col>
+              <Header title="Contact Me" fontSize={2} verticalMargin={3} />
+              <List />
+            </Col>
+          </Row>
+        </FadeComponent>
       </Col>
     </>
   );
