92f60d552448a5ce5c007816b8a72b8ef3e9e843
Joseph Soria <josephsoria6@gmail.com>
2021-09-18 10:46:36 -0500

    Made listObjects file to store objects for importing to components where mapping is needed

--- src/components/List.js
+++ src/components/List.js
@@ -1,55 +1,17 @@
 import ListGroup from "react-bootstrap/ListGroup";
-import { useState } from "react";
-import {
-  InboxFill,
-  PhoneVibrateFill,
-  Github,
-  Linkedin,
-} from "react-bootstrap-icons";
 
-const List = () => {
-  const [transformText, setTransformText] = useState(false);
-
-  const handleHover = () => {
-    setTransformText(!transformText);
-  };
-
-  const handleHLeaveHover = () => {
-    setTransformText(!transformText);
-  };
-
+const List = ({ listItems }) => {
   return (
     <ListGroup className="text-center lh-sm">
-      <ListGroup.Item>
-        <InboxFill size={24} />
-        Email:{" "}
-        <a href="mailto: josephsoria6@gmail.com">josephsoria6@gmail.com</a>
-      </ListGroup.Item>
-      <ListGroup.Item
-        onMouseEnter={handleHover}
-        onMouseLeave={handleHLeaveHover}
-      >
-        <PhoneVibrateFill size={24} />
-        Phone: {!transformText ? "Hover to display!" : "832-657-9060"}
-      </ListGroup.Item>
-      <ListGroup.Item>
-        <Github size={24} />
-        Github:{" "}
-        <a href="https://github.com/Joeseff6" target="_blank" rel="noreferrer">
-          See my repos!
-        </a>
-      </ListGroup.Item>
-      <ListGroup.Item>
-        <Linkedin size={24} />
-        LinkedIn:{" "}
-        <a
-          href="https://linkedin.com/in/joseph-soria-5a335845/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Connect with me!
-        </a>
-      </ListGroup.Item>
+      {listItems.map((listItem) => {
+        return (
+          <ListGroup.Item key={listItem.itemText}>
+            <listItem.icon size={24} />
+            {listItem.itemText}
+            <a href={listItem.link} target="_blank" rel="noreferrer">{listItem.linkText}</a>
+          </ListGroup.Item>
+        );
+      })}
     </ListGroup>
   );
 };
