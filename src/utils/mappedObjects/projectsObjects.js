507015b03e46880e3139f7aa972399634c4800ae
Joseph Soria <josephsoria6@gmail.com>
2021-09-19 12:44:15 -0500

    Made array of objects for project images

---
+++ src/utils/mappedObjects/projectsObjects.js
@@ -0,0 +1,43 @@
+import StayRnb from "../../assets/images/StayRnb.JPG"
+import Suitcase from "../../assets/images/suitcase.PNG"
+import WeatherCentral from "../../assets/images/weatherCentral.PNG"
+import SongNSeek from "../../assets/images/SongNSeek.JPG"
+import TechBlog from "../../assets/images/TechBlog.PNG"
+import FullStackQuiz from "../../assets/images/fullStackQuiz.PNG"
+import GoogleBooks from "../../assets/images/GoogleBooks.JPG"
+
+
+const projectImages = [
+  {
+    image: StayRnb,
+    alt: "A headshot of me with a blurred background",
+  },
+  {
+    image: SongNSeek,
+    alt: "A headshot of me with a blurred background",
+  },
+  {
+    image: Suitcase,
+    alt: "A headshot of me with a blurred background",
+  },
+  {
+    image: TechBlog,
+    alt: "A headshot of me with a blurred background",
+  },
+  {
+    image: GoogleBooks,
+    alt: "A headshot of me with a blurred background",
+  },
+  {
+    image: WeatherCentral,
+    alt: "A headshot of me with a blurred background",
+  },
+  {
+    image: FullStackQuiz,
+    alt: "A headshot of me with a blurred background",
+  },
+]
+
+export {
+  projectImages,
+}
