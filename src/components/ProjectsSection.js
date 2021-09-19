148ee159652e38de20679777d3d7e51c42d72023
Joseph Soria <josephsoria6@gmail.com>
2021-09-19 12:53:11 -0500

    Added a header for the ProjectsSection

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -3,15 +3,19 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ImageCarousel from "./ImageCarousel";
 import { projectImages } from "../utils/mappedObjects/projectsObjects";
-
+import Header from "./Header";
 
 const ProjectsSection = () => {
   return (
     <>
       <Col className="p-0" md={3}>
-        <ImageCarousel images={projectImages}/>
+        <ImageCarousel images={projectImages} />
       </Col>
       <Col>
+        <Row>
+          <Header title="My Projects" heading={1}/>
+        </Row>
+        <Row></Row>
       </Col>
     </>
   );
