5ee69bd6320d67e759835ab4e008708a117fc015
Joseph Soria <josephsoria6@gmail.com>
2021-09-19 12:28:03 -0500

    Changed content in return statement

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import ImageCarousel from "./ImageCarousel";
+
 
 const ProjectsSection = () => {
   return (
-    <div>
-      <h1>This is the Projects section</h1>
-    </div>
+    <>
+      <Col md={3}>
+        <ImageCarousel />
+      </Col>
+      <Col>
+      </Col>
+    </>
   );
 };
 
