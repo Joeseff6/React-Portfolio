845cbe2cdcaac227cda51f3a8d99a8a0945463f0
Joseph Soria <josephsoria6@gmail.com>
2021-09-18 21:43:25 -0500

    Added ternary in case an icon isn't passed as a prop

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,13 @@
 import ListGroup from "react-bootstrap/ListGroup";
 
 const List = ({ listItems }) => {
+  console.log(listItems)
   return (
     <ListGroup className="text-center lh-sm">
       {listItems.map((listItem) => {
         return (
           <ListGroup.Item key={listItem.itemText}>
-            <listItem.icon size={24} />
+            {listItem.icon ? <listItem.icon size={24} /> : ""}
             {listItem.itemText}
             <a href={listItem.link} target="_blank" rel="noreferrer">{listItem.linkText}</a>
           </ListGroup.Item>
