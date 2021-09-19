6beb87b7305606593b557dc15a3d092ebaa2ee6d
Joseph Soria <josephsoria6@gmail.com>
2021-09-19 12:43:58 -0500

    Imported projectImages, passed as prop to carousel

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -2,13 +2,14 @@ import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ImageCarousel from "./ImageCarousel";
+import { projectImages } from "../utils/mappedObjects/projectsObjects";
 
 
 const ProjectsSection = () => {
   return (
     <>
-      <Col md={3}>
-        <ImageCarousel />
+      <Col className="p-0" md={3}>
+        <ImageCarousel images={projectImages}/>
       </Col>
       <Col>
       </Col>
