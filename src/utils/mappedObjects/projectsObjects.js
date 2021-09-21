c25e3863c5a46353915844e019b344a76859c4e9
Joseph Soria <josephsoria6@gmail.com>
2021-09-20 22:11:28 -0500

    Made a projects object

--- src/utils/mappedObjects/projectsObjects.js
+++ src/utils/mappedObjects/projectsObjects.js
@@ -1,11 +1,10 @@
-import StayRnb from "../../assets/images/StayRnb.JPG"
-import Suitcase from "../../assets/images/suitcase.PNG"
-import WeatherCentral from "../../assets/images/weatherCentral.PNG"
-import SongNSeek from "../../assets/images/SongNSeek.JPG"
-import TechBlog from "../../assets/images/TechBlog.PNG"
-import FullStackQuiz from "../../assets/images/fullStackQuiz.PNG"
-import GoogleBooks from "../../assets/images/GoogleBooks.JPG"
-
+import StayRnb from "../../assets/images/StayRnb.JPG";
+import Suitcase from "../../assets/images/suitcase.PNG";
+import WeatherCentral from "../../assets/images/weatherCentral.PNG";
+import SongNSeek from "../../assets/images/SongNSeek.JPG";
+import TechBlog from "../../assets/images/TechBlog.PNG";
+import FullStackQuiz from "../../assets/images/fullStackQuiz.PNG";
+import GoogleBooks from "../../assets/images/GoogleBooks.JPG";
 
 const projectImages = [
   {
@@ -36,8 +35,41 @@ const projectImages = [
     image: FullStackQuiz,
     alt: "A screenshot of the homepage for my Full Stack Quiz app.",
   },
-]
+];
 
-export {
-  projectImages,
-}
+const projects = [
+  {
+    image: StayRnb,
+    alt: "A screenshot of the homepage for my Stay R n b app.",
+    title: "Project title",
+    description: "A project description",
+    repoLink: "The repo link",
+    deployedLink: "Link where project is deployed",
+  },
+  {
+    image: SongNSeek,
+    alt: "A screenshot of the homepage for my Song N Seek app.",
+    title: "Project title",
+    description: "A project description",
+    repoLink: "The repo link",
+    deployedLink: "Link where project is deployed",
+  },
+  {
+    image: Suitcase,
+    alt: "A screenshot of the homepage for my Suitcase app.",
+    title: "Project title",
+    description: "A project description",
+    repoLink: "The repo link",
+    deployedLink: "Link where project is deployed",
+  },
+  {
+    image: TechBlog,
+    alt: "A screenshot of the posts page for my Tech Blog app.",
+    title: "Project title",
+    description: "A project description",
+    repoLink: "The repo link",
+    deployedLink: "Link where project is deployed",
+  },
+];
+
+export { projectImages, projects };
