7ccd1140df189b623082c4259b2a49761e5380b8
Joseph Soria <josephsoria6@gmail.com>
2021-09-20 21:50:34 -0500

    Gave state for pagination, imported projects, passed projects as a prop to ProjectTiles

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ImageCarousel from "./ImageCarousel";
-import { projectImages } from "../utils/mappedObjects/projectsObjects";
+import {
+  projectImages,
+  projects,
+} from "../utils/mappedObjects/projectsObjects";
 import Header from "./Header";
+import Button from "react-bootstrap/Button";
+import ProjectTiles from "./ProjectTiles";
 
 const ProjectsSection = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [projectsPerPage, setProjectsPerPage] = useState(3);
+
+  const indexOfLastProject = currentPage * projectsPerPage;
+  const indexOfFirstProject = indexOfLastProject - projectsPerPage;
+  const currentProjects = projects.slice(
+    indexOfFirstProject,
+    indexOfLastProject
+  );
+
   return (
     <>
       <Col className="p-0" md={3}>
@@ -13,9 +28,11 @@ const ProjectsSection = () => {
       </Col>
       <Col>
         <Row>
-          <Header title="My Projects" heading={1}/>
+          <Header title="My Projects" heading={1} />
         </Row>
-        <Row></Row>
+        <Row className="px-2" style={{ height: "298px" }}>
+          <ProjectTiles projects={currentProjects}/>
+        </Row>
       </Col>
     </>
   );
