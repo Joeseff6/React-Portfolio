0d7da8a2b06b363e467dc7595a4affbf53706c12
Joseph Soria <josephsoria6@gmail.com>
2021-09-25 11:53:07 -0500

    Changed name of state activeNumber, wrote onPageChange, passed onPageChange as callback function in onClick

--- src/components/TilePagination.js
+++ src/components/TilePagination.js
@@ -4,15 +4,19 @@ import Pagination from "react-bootstrap/Pagination";
 class TilePagination extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { pageNumber: 1, items: [] };
+  }
 
-    this.state = { activeNumber: null, items: [] };
+  onPageChange = async (e) => {
+    await this.setState({ pageNumber: parseInt(e.target.dataset.number)});
+    this.props.handleClick(this.state.pageNumber);
   }
 
   updatePagination() {
     let paginationArr = []
     for (let i = 1; i <= this.props.numberOfPages; i++) {
       paginationArr.push(
-        <Pagination.Item key={i} active={i === this.state.activeNumber} data-number={i} onClick={(e) => this.setState({ activeNumber: parseInt(e.target.dataset.number)})}>
+        <Pagination.Item key={i} active={i === this.state.pageNumber} data-number={i} onClick={(this.onPageChange)}>
           {i}
         </Pagination.Item>
       );
@@ -25,7 +29,7 @@ class TilePagination extends React.Component {
   };
 
   componentDidUpdate( previousProps, previousState) {
-    if (previousState.activeNumber !== this.state.activeNumber) {
+    if (previousState.pageNumber !== this.state.pageNumber) {
       this.updatePagination();
     }
   }
