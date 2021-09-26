9310ea69a0792203f6668364871b781e2958b7ce
Joseph Soria <josephsoria6@gmail.com>
2021-09-26 12:04:08 -0500

    Wrote logic to show projects

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -3,14 +3,15 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { projects } from "../utils/mappedObjects/projectsObjects";
 import Header from "./Header";
-import ProjectCards from "./ProjectCards";
+import ProjectCard from "./ProjectCard";
 import ProjectTiles from "./ProjectTiles";
 import TilePagination from "./TilePagination";
 import Button from "react-bootstrap/Button";
-import { CaretLeftFill, CaretRightFill } from "react-bootstrap-icons";
 
 const ProjectsSection = () => {
-  const [currentPage, setCurrentPage] = useState(1);
+  const [ currentPage, setCurrentPage ] = useState(1);
+  const [ project, setProject ] = useState(projects[0]);
+  const [ show, setShow ] = useState(false);
 
   const projectsPerPage = 3;
   const indexOfLastProject = currentPage * projectsPerPage;
@@ -21,11 +22,11 @@ const ProjectsSection = () => {
   );
   const numberOfPages = Math.ceil(projects.length / projectsPerPage);
 
-  const paginationPageChange = (pageNumber) => {
+  const handlePaginationPageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
-  const buttonPageChange = (e) => {
+  const handleButtonPageChange = (e) => {
     if (
       (e.target.dataset.direction === "left" && currentPage === 1) ||
       (e.target.dataset.direction === "right" && currentPage === numberOfPages)
@@ -40,17 +41,22 @@ const ProjectsSection = () => {
     
   };
 
+  const handleProjectSelection = (id) => {
+    setProject(projects[id]);
+    setShow(true);
+  }
+
   return (
     <Row className="my-5 project-section">
       <Col>
         <Row>
           <Header title="My Projects" heading={1} bottomMargin={4} />
         </Row>
-        <Row className="justify-content-center mb-5 py-4 project-tile-row">
+        <Row className="justify-content-center mb-4 py-4 project-tile-row">
           <Col>
             <Row className="mb-3">
               <Col className="d-flex justify-content-end">
-                <Button className="paginationButton" data-direction="left" onClick={buttonPageChange}>
+                <Button className="paginationButton" data-direction="left" onClick={handleButtonPageChange}>
                   <span data-direction="left">Previous</span>
                 </Button>
               </Col>
@@ -60,23 +66,22 @@ const ProjectsSection = () => {
               >
                 <TilePagination
                   numberOfPages={numberOfPages}
-                  handleClick={paginationPageChange}
+                  handleClick={handlePaginationPageChange}
                   currentPage={currentPage}
                 />
               </Col>
               <Col className="d-flex justify-content-start">
-                <Button className="paginationButton" data-direction="right" onClick={buttonPageChange}>
+                <Button className="paginationButton" data-direction="right" onClick={handleButtonPageChange }>
                 <span data-direction="right">Next</span>
                 </Button>
               </Col>
             </Row>
             <Row>
-              <ProjectTiles projects={currentProjects} />
+              <ProjectTiles projects={currentProjects} handleClick={handleProjectSelection}/>
             </Row>
           </Col>
         </Row>
-
-        {/* <ProjectCards projects={currentProjects} /> */}
+        {show && <ProjectCard project={project} />}
       </Col>
     </Row>
   );
