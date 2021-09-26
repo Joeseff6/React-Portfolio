098a1916207292ad7459375a5fba55a8d9aec0fd
Joseph Soria <josephsoria6@gmail.com>
2021-09-26 12:04:50 -0500

    Removed index parameter from map, replaced with id

--- src/components/ProjectTiles.js
+++ src/components/ProjectTiles.js
@@ -2,19 +2,19 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 
-const ProjectTiles = ({ projects }) => {
+const ProjectTiles = ({ projects, handleClick }) => {
   return (
     <>
       {projects.map((project) => {
         return (
-          <Col key={project.title}>
+          <Col key={project.id}>
             <img
               src={project.image}
               alt={project.alt}
               className="project-tile-image d-block mx-auto"
             />
-            <Button className="mt-1 d-block mx-auto">
-              <span>{project.title}</span>
+            <Button className="mt-1 d-block mx-auto" data-id={project.id} onClick={(e) => handleClick(e.target.dataset.id)}>
+              <span data-id={project.id}>{project.title}</span>
             </Button>
           </Col>
         );
