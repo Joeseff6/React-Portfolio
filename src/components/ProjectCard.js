1915b9d74751eb9fd0d509d3fb5a48d110308cbd
Joseph Soria <josephsoria6@gmail.com>
2021-09-26 12:03:48 -0500

    Removed old code, import CloseButton

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,36 +1,12 @@
-import Button from "react-bootstrap/Button";
-import StayRnb from "../assets/images/StayRnb.JPG";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
+import CloseButton from "react-bootstrap/CloseButton";
 
 const ProjectCard = ({ project }) => {
-  // return (
-  //   <Col className="d-flex align-items-center justify-content-center">
-  //     <Card className="mx-4">
-  //       <Card.Img src={project.image} alt={project.alt} />
-  //       <Card.Body className="p-2">
-  //         <Card.Title className="mb-1 text-center">{project.title}</Card.Title>
-  //         <Card.Text className="mb-1">
-  //           Repo Link:{" "}
-  //           <a href={project.repoLink} target="_blank" rel="noreferrer">
-  //             {project.repoLink}
-  //           </a>
-  //         </Card.Text>
-  //         <Card.Text className="mb-1">
-  //           Deployed Link:{" "}
-  //           <a href={project.deployedLink} target="_blank" rel="noreferrer">
-  //             {project.deployedLink}
-  //           </a>
-  //         </Card.Text>
-  //         <Card.Text className="mb-1">{project.description}</Card.Text>
-  //       </Card.Body>
-  //     </Card>
-  //   </Col>
-  // );
-
   return (
-    <Col className="d-flex align-items-center justify-content-center">
+    <Col className="d-flex align-items-center justify-content-center mb-2">
       <Card className="mx-4">
+        <CloseButton />
         <p className="fs-1 text-center my-3">{project.title}</p>
         <p className="text-center mb-2">
           Github Repo:{" "}
