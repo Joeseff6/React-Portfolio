2f9ab564ccb455e5e32c84515122932c8cb579f2
Joseph Soria <josephsoria6@gmail.com>
2021-09-26 12:03:32 -0500

    Changed the key prop for carousel images

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -4,9 +4,9 @@ import Carousel from "react-bootstrap/Carousel";
 const ImageCarousel = ({ images }) => {
   return (
     <Carousel controls={false} indicators={false} interval={4000} pause={false}>
-    {images.map((image) => {
+    {images.map((image, index) => {
       return (
-        <Carousel.Item key={image.alt}>
+        <Carousel.Item key={index}>
           <img src={image.image} alt={image.alt} className="w-100 carouselImage" />
         </Carousel.Item>
       );
