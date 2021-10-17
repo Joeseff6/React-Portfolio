78c17ddaa85023f303a63a85ca07409a69d9b614
Joseph Soria <josephsoria6@gmail.com>
2021-10-17 18:08:30 -0500

    Added navbar brand as "Joseph Soria"

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
-import Resume from "../assets/files/JosephSoriaResume.pdf"
+import Resume from "../assets/files/JosephSoriaResume.pdf";
 
 class Navbar extends React.Component {
   render() {
     return (
-      <Nav
-        activeKey="/home"
-      >
+      <Nav activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="#" className="navbar-brand">Joseph Soria</Nav.Link>
+        </Nav.Item>
         <Nav.Item>
           <Nav.Link href="#about-me">About Me</Nav.Link>
         </Nav.Item>
@@ -18,7 +19,9 @@ class Navbar extends React.Component {
           <Nav.Link href="#projects">Projects</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href={Resume} target="_blank" rel="noreferrer">My Resume</Nav.Link>
+          <Nav.Link href={Resume} target="_blank" rel="noreferrer">
+            My Resume
+          </Nav.Link>
         </Nav.Item>
       </Nav>
     );
