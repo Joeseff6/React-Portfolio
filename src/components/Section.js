0ad648b744c3ed93e7a43116a50cc626019e7e83
Joseph Soria <josephsoria6@gmail.com>
2021-10-17 18:09:52 -0500

    Removed fadeDirection prop

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,15 +3,11 @@ import Row from "react-bootstrap/Row";
 class Section extends React.Component {
   render() {
     return (
-      <Row className={`section-fade-${this.props.fadeDirection} my-5 justify-content-center`}>
+      <Row className="section-row my-5 justify-content-center">
         {this.props.children}
       </Row>
     )
   }
-}
-
-Section.defaultProps = {
-  fadeDirection: "right",
 }
 
 export default Section;
