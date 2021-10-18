0c7a422511ac5f9340038035bfa169018450db29
Joseph Soria <josephsoria6@gmail.com>
2021-10-17 19:36:02 -0500

    Put a conditional render statement if project deployed link exists

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,23 +6,28 @@ const ProjectCard = ({ project, handleClick }) => {
   return (
     <Col className="d-flex align-items-center justify-content-center mb-2">
       <Card className="mx-4">
-        <CloseButton onClick={handleClick}/>
-        <p className="fs-1 text-center my-3" id={project.id}>{project.title}</p>
+        <CloseButton onClick={handleClick} />
+        <p className="fs-1 text-center my-3" id={project.id}>
+          {project.title}
+        </p>
         <p className="text-center mb-2">
           Github Repo:{" "}
           <a href={project.repoLink} target="_blank" rel="noreferrer">
             {project.repoLink}
           </a>
         </p>
-        <p className="text-center mb-2">
-          Deployed Link:{" "}
-          <a href={project.deployedLink} target="_blank" rel="noreferrer">
-            {project.deployedLink}
-          </a>
-        </p>
+        {project.deployedLink && (
+          <p className="text-center mb-2">
+            Deployed Link:{" "}
+            <a href={project.deployedLink} target="_blank" rel="noreferrer">
+              {project.deployedLink}
+            </a>
+          </p>
+        )}
+
         <p className="text-center mb-2">Skills used: {project.skills}</p>
         <Card.Img src={project.image} alt={project.alt} />
-        <p className="my-3 px-4">{project.description}</p>
+        <p className="my-3 px-5 lh-base">{project.description}</p>
       </Card>
     </Col>
   );
